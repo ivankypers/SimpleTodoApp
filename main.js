cb4623176bd2c16a19c6f49f2dcc0b52034ee47d
CyberGnida <ivan.mol.10@mail.ru>
2023-07-03 16:57:14 +0300

    almost full todoApp

--- main.js
+++ main.js
@@ -34,6 +34,37 @@ function createTodoList() {
     return list
 }
 
+
+
+function createTodoItem(name) {
+    let item = document.createElement('li');
+
+    let buttonGroup = document.createElement('div');
+    let doneButton = document.createElement('button');
+    let deleteButton = document.createElement('button');
+
+    item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+    item.textContent = name;
+
+    buttonGroup.classList.add('btn-group', 'btn-group-sm')
+    doneButton.classList.add('btn', 'btn-success')
+    doneButton.textContent = 'Готово'
+    deleteButton.classList.add('btn', 'btn-danger')
+    deleteButton.textContent = 'Удалить'
+
+    buttonGroup.append(doneButton);
+    buttonGroup.append(deleteButton);
+    item.append(buttonGroup)
+
+    return {
+        item,
+        doneButton,
+        deleteButton
+    }
+
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     let container = document.getElementById('todo-app')
 
@@ -44,4 +75,28 @@ document.addEventListener('DOMContentLoaded', () => {
     container.append(todoAppTitle);
     container.append(todoItemForm.form);
     container.append(todoList);
+
+    todoItemForm.form.addEventListener('submit', function (e) {
+        e.preventDefault()
+
+        if (!todoItemForm.input.value) {
+            return;
+        }
+
+        let todoItem = createTodoItem(todoItemForm.input.value);
+
+        todoItem.doneButton.addEventListener('click', function () {
+            todoItem.item.classList.toggle('list-group-item-success')
+        });
+        todoItem.deleteButton.addEventListener('click', function () {
+            if (confirm('Вы уверены?')) {
+                todoItem.item.remove()
+            }
+
+        })
+
+        todoList.append(todoItem.item);
+        todoItemForm.input.value = '';
+
+    })
 })
