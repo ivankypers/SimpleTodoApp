7ba4d13e7281433b10957205f8815c6a743a07ba
CyberGnida <ivan.mol.10@mail.ru>
2023-07-06 14:24:41 +0300

    convert each todoItem to object with id. Added array where each task add like an object

--- main.js
+++ main.js
@@ -1,52 +1,73 @@
 function createAppTitle(title) {
     let appTitle = document.createElement('h2');
     appTitle.innerHTML = title;
-    return appTitle;
+    return appTitle; //Создаем элемент заголовка
+
 }
 
 function createTodoItemForm() {
-    let form = document.createElement('form');
-    let input = document.createElement('input');
+    let form = document.createElement('form'); //Создание основной формы
+    let input = document.createElement('input'); //инпут формы
     let buttonWrapper = document.createElement('div');
-    let button = document.createElement('button');
+    let button = document.createElement('button'); //кнопка формы для создания li
 
     form.classList.add('input-group', 'mb-3');
     input.classList.add('form-control');
     input.placeholder = 'Введите название нового дела :)'
     buttonWrapper.classList.add('input-group-append')
     button.classList.add('btn', 'btn-primary');
-    button.textContent = 'Добавить дело'
+    button.textContent = 'Добавить дело'  // Добавляем классы и название
+
+    button.disabled = true;
+    input.addEventListener('input', () => {
+        button.disabled = input.value === '';
+    })
 
     buttonWrapper.append(button);
     form.append(input);
-    form.append(buttonWrapper);
+    form.append(buttonWrapper);  //Добавляем кнопку в контейнер -> инпут в форму -> контейнер с кнопкой
 
     return {
         form,
         input,
-        button,
+        button, //Возвращаем все из функции в dom
     };
 }
 
 function createTodoList() {
-    let list = document.createElement('ul')
+
+    let list = document.createElement('ul')  //Создаем лист, куда будут добавляться элементы li TodoItem
     list.classList.add('list-group')
     return list
 }
 
 
+let todos = [];
+
 
 function createTodoItem(name) {
-    let item = document.createElement('li');
 
-    let buttonGroup = document.createElement('div');
+    let id = todos.length + 1;
+    let todo = {
+        id: id,
+        name: name,
+        done: false
+    };
+
+
+
+    let item = document.createElement('li'); //создаем li
+
+    let buttonGroup = document.createElement('div');  //btn container
     let doneButton = document.createElement('button');
-    let deleteButton = document.createElement('button');
+    let deleteButton = document.createElement('button'); //и две кнопки "готово" "удалить"
 
-    item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
-    item.textContent = name;
+    item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center'); //классы для распределения текста и кнопок в элементе списка
+    item.textContent = todo.name;
+    item.id = todo.id
 
-    buttonGroup.classList.add('btn-group', 'btn-group-sm')
+    todos.push(todo)
+    buttonGroup.classList.add('btn-group', 'btn-group-sm')  //классы для контейнера кнопок и для самих кнопок
     doneButton.classList.add('btn', 'btn-success')
     doneButton.textContent = 'Готово'
     deleteButton.classList.add('btn', 'btn-danger')
@@ -57,6 +78,7 @@ function createTodoItem(name) {
     item.append(buttonGroup)
 
     return {
+        todo,
         item,
         doneButton,
         deleteButton
@@ -67,12 +89,12 @@ function createTodoItem(name) {
 
 
 
+
 function createTodoApp(container, title= 'Список дел') {
 
         let todoAppTitle = createAppTitle(title)
         let todoItemForm = createTodoItemForm()
         let todoList = createTodoList()
-
         container.append(todoAppTitle);
         container.append(todoItemForm.form);
         container.append(todoList);
@@ -84,16 +106,27 @@ function createTodoApp(container, title= 'Список дел') {
                 return;
             }
 
+
             let todoItem = createTodoItem(todoItemForm.input.value);
 
             todoItem.doneButton.addEventListener('click', function () {
                 todoItem.item.classList.toggle('list-group-item-success')
+                for (let obj in todos) {
+                    if (todos[obj].id === Number(todoItem.item.id)) {
+                        todos[obj].done = todoItem.item.classList.contains('list-group-item-success');
+                    }
+                }
             });
             todoItem.deleteButton.addEventListener('click', function () {
                 if (confirm('Вы уверены?')) {
                     todoItem.item.remove()
+                    for (let i = 0; i < todos.length; i++) {
+                        if (todos[i].id === Number(todoItem.item.id)) {
+                            todos.splice(i, 1);
+                            break;
+                        }
+                    }
                 }
-
             })
 
             todoList.append(todoItem.item);
@@ -105,3 +138,4 @@ function createTodoApp(container, title= 'Список дел') {
 
 
 window.createTodoApp = createTodoApp;
+
